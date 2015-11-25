e5c5b6bc5e53c47f01ec21ebd9409b1af6bc06c7
Javier Santos <me@javiersantos.me>
2015-11-25 23:34:09 +0100

    Add Sample Keys class

---
+++ app/src/main/java/com/javiersantos/moticons/Keys.java
@@ -0,0 +1,15 @@
+package com.javiersantos.moticons;
+
+public class Keys {
+    public static String getTestDevice() {
+        return "<UNIQUE_DEVICE_ID_FOR_TESTING>";
+    }
+
+    public static String getBase64EncodedPublicKey() {
+        return "<IAP_PURCHASE_PUBLIC_KEY>";
+    }
+
+    public static String getHeyZapPublicKey() {
+        return "<HEYZAP_PUBLIC_KEY>";
+    }
+}
